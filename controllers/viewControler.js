8ca95266f09ba1450c3b9a5c946dc70d64b298d6
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-09-13 21:02:59 +0200

    added card template in overview

--- controllers/viewControler.js
+++ controllers/viewControler.js
@@ -1,6 +1,14 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All tours' });
-};
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  //* Get tour data from collection
+  const tours = await Tour.find();
+
+  //* Build template
+  //* Render that template using tour data
+  res.status(200).render('overview', { title: 'All tours', tours });
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', { title: 'The Forest Hiker Tour' });
