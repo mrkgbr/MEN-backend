903871fafaa8e9a7465470ccb726acc3c735f3fa
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-09-13 18:52:09 +0200

    refactoring views

---
+++ controllers/viewControler.js
@@ -0,0 +1,7 @@
+exports.getOverview = (req, res) => {
+  res.status(200).render('overview', { title: 'All tours' });
+};
+
+exports.getTour = (req, res) => {
+  res.status(200).render('tour', { title: 'The Forest Hiker Tour' });
+};
