726824cc57c00615bbab5e6f5368f32459a8d221
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-09-14 17:47:56 +0200

    added tour template

--- controllers/viewControler.js
+++ controllers/viewControler.js
@@ -2,14 +2,16 @@ const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res) => {
-  //* Get tour data from collection
   const tours = await Tour.find();
-
-  //* Build template
-  //* Render that template using tour data
   res.status(200).render('overview', { title: 'All tours', tours });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', { title: 'The Forest Hiker Tour' });
-};
+exports.getTour = catchAsync(async (req, res) => {
+  console.log(req.params.slug);
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  res.status(200).render('tour', { title: 'The Forest Hiker Tour', tour });
+});
