a0f82335212f879ca4779537e06a8db74c0eedb6
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-09-23 13:46:22 +0200

    removed development url

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -2216,9 +2216,7 @@
   );
   var bookTour = async (tourId) => {
     try {
-      const session = await axios_default(
-        `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-      );
+      const session = await axios_default(`/api/v1/bookings/checkout-session/${tourId}`);
       await stripe.redirectToCheckout({
         sessionId: session.data.session.id
       });
