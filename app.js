9ee3e53e5c153346ed582f55bc722b85cb24419b
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-09-23 15:15:17 +0200

    add heroku proxy settings, commented out for later use

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
+// Use it with heroku
+// app.enable('trust proxy');
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
